f81fbf166f3bd424101cb067096b577a5f19722c
betogroo <luizhumberto@gmail.com>
2024-08-18 20:24:09 -0300

    feat: create 404 nof found page

---
+++ app/pages/[...-404].vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex flex-col items-center justify-center h-screen">
+    <h1 class="text-9xl font-semibold">404</h1>
+    <h6 class="text-4xl">Oops! Algo deu errado</h6>
+    <UButton
+      to="/"
+      color="black"
+      variant="outline"
+      class="mt-6"
+      leading-icon="i-heroicons:arrow-uturn-left-16-solid"
+      >Voltar</UButton
+    >
+  </div>
+</template>
