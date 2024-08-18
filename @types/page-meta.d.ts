14574343944ed9dd00cc288ad5f54b783a0edb2e
betogroo <luizhumberto@gmail.com>
2024-08-18 19:27:10 -0300

    chore: create page-meta types

---
+++ @types/page-meta.d.ts
@@ -0,0 +1,13 @@
+// types/meta.d.ts
+import { type PageMeta } from 'nuxt/app'
+
+declare module 'nuxt/app' {
+  interface PageMeta {
+    title: string
+    showInNavBar?: boolean
+    hideNavBar?: boolean
+    icon?: string
+    requiresAuth?: boolean
+    order?: number
+  }
+}
