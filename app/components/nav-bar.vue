993f6221bc2af9caf98901181bb075a3447b34ed
betogroo <luizhumberto@gmail.com>
2024-08-18 19:38:17 -0300

    refactor: move nav-bar to component

---
+++ app/components/nav-bar.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+  <nav class="flex justify-end space-x-2 mx-3">
+    <ULink
+      active-class="font-semibold"
+      class="hover:underline"
+      to="/"
+      >Home</ULink
+    >
+    <div>|</div>
+    <ULink
+      class="hover:underline"
+      active-class="font-semibold"
+      to="/about"
+      >About</ULink
+    >
+  </nav>
+</template>
