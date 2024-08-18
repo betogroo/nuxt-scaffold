b09833a0dfa2efbcab9f6653d255f5cf649850cc
betogroo <luizhumberto@gmail.com>
2024-08-18 15:51:10 -0300

    feat: create index and about pages

---
+++ app/pages/about.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+  definePageMeta({
+    showInNavBar: true,
+    requiresAuth: true,
+    title: 'About',
+  })
+</script>
+
+<template>
+  <h1>About</h1>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste quibusdam
+    reprehenderit fugit eaque nemo, tempore ea, porro recusandae neque tenetur
+    velit natus molestiae fuga voluptatibus eveniet sint ex at necessitatibus.
+  </p>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+  definePageMeta({
+    showInNavBar: true,
+    requiresAuth: true,
+    title: 'Home',
+  })
+</script>
+
+<template>
+  <h1>Home</h1>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste quibusdam
+    reprehenderit fugit eaque nemo, tempore ea, porro recusandae neque tenetur
+    velit natus molestiae fuga voluptatibus eveniet sint ex at necessitatibus.
+  </p>
+</template>
