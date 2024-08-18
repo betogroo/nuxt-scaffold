1477c8f4f58640e03d604f88fdd62a1bfe85089a
betogroo <luizhumberto@gmail.com>
2024-08-18 16:09:41 -0300

    feat: format h1 and p using tailwind

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -7,8 +7,8 @@
 </script>
 
 <template>
-  <h1>About</h1>
-  <p>
+  <h1 class="font-semibold text-3xl mt-4 ml-4">About</h1>
+  <p class="text-justify mx-4">
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste quibusdam
     reprehenderit fugit eaque nemo, tempore ea, porro recusandae neque tenetur
     velit natus molestiae fuga voluptatibus eveniet sint ex at necessitatibus.
