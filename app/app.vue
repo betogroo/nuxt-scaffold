f7eae7b084c5f6990f1c3f2c72736f0e44c2b0e3
betogroo <luizhumberto@gmail.com>
2024-08-18 16:35:54 -0300

    feat: create simple navbar whit 2 nuxt ui links

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,22 @@
 <template>
   <div>
+    <header>
+      <nav class="flex justify-end space-x-2 mx-3">
+        <ULink
+          active-class="font-semibold"
+          class="hover:underline"
+          to="/"
+          >Home</ULink
+        >
+        <div>|</div>
+        <ULink
+          class="hover:underline"
+          active-class="font-semibold"
+          to="/about"
+          >About</ULink
+        >
+      </nav>
+    </header>
     <NuxtPage />
   </div>
 </template>
--- app/pages/about.vue
+++ app/pages/about.vue
@@ -7,10 +7,11 @@
 </script>
 
 <template>
-  <h1 class="font-semibold text-3xl mt-4 ml-4">About</h1>
+  <h1 class="font-semibold text-3xl my-4 ml-4">About</h1>
   <p class="text-justify mx-4">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste quibusdam
-    reprehenderit fugit eaque nemo, tempore ea, porro recusandae neque tenetur
-    velit natus molestiae fuga voluptatibus eveniet sint ex at necessitatibus.
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta recusandae
+    atque ratione magnam, deleniti distinctio expedita, dolorum ab explicabo eum
+    ut reprehenderit quaerat consequuntur animi sunt, odio laboriosam
+    temporibus! Sint!
   </p>
 </template>
