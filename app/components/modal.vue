f0b9b455c411047af598e30555bc41d26d6d4f0e
betogroo <luizhumberto@gmail.com>
2024-08-27 16:48:24 -0300

    feat: add modal

--- app/components/modal.vue
+++ app/components/modal.vue
@@ -1,19 +1,64 @@
-<script setup lang="ts">
+<script setup lang="ts" generic="T">
+  export interface FormState {
+    name: string
+    email: string
+  }
   interface Props {
     title?: string
   }
   defineProps<Props>()
+
+  const $emit = defineEmits<{
+    submit: [state: typeof state]
+    close: []
+  }>()
+
+  const state = ref<FormState>({
+    name: '',
+    email: '',
+  })
+
+  const addData = () => {
+    $emit('submit', state)
+  }
+
+  const close = () => {
+    $emit('close')
+  }
 </script>
 
 <template>
-  <UModal>
+  <UModal width="25">
     <UCard>
       <template #header>
-        <h1 class="page-title">{{ title }}</h1>
+        <h1 class="text-2xl font-semibold">{{ title }}</h1>
       </template>
-      <div class="space-y-2">
-        <p>This modal was opened programmatically !</p>
-      </div>
+      <UForm
+        :state="state"
+        @submit="addData"
+      >
+        <UFormGroup label="Nome">
+          <UInput v-model="state.name" />
+        </UFormGroup>
+
+        <UFormGroup label="Email">
+          <UInput v-model="state.email" />
+        </UFormGroup>
+      </UForm>
+      <template #footer>
+        <div class="flex justify-end space-x-4">
+          <UButton
+            label="Enviar"
+            type="submit"
+          />
+          <UButton
+            color="red"
+            label="Fechar"
+            variant="outline"
+            @click="close"
+          />
+        </div>
+      </template>
     </UCard>
   </UModal>
 </template>
