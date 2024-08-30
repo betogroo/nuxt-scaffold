1d8819b7a96bf3d34cde3a5db94923ef44466776
betogroo <luizhumberto@gmail.com>
2024-08-30 11:02:42 -0300

    refactor: change fake users type do view form and add id and created_at fake generator

--- app/composables/useRegistration.ts
+++ app/composables/useRegistration.ts
@@ -1,8 +1,8 @@
 import type { ZodError } from 'zod'
-import { addFormSchema, type AddForm } from '~/models/form'
-const { delay } = useHelpers()
+import { addFormSchema, type AddForm, type ViewForm } from '~/models/form'
+const { delay, getRandomUUID } = useHelpers()
 
-const fakeUsers = ref<AddForm[]>([])
+const fakeUsers = ref<ViewForm[]>([])
 const isPending = ref<boolean>(false)
 const useRegistration = () => {
   const addUser = async (data: AddForm) => {
@@ -11,9 +11,13 @@ const useRegistration = () => {
     await delay(2000, 'Testing addData')
     try {
       const parsedData = addFormSchema.parse(data)
-      //throw new Error('Erro simulado no cadastro')
-      fakeUsers.value.push(parsedData)
       if (!parsedData) throw Error('Não foi possível cadastrar')
+      //throw new Error('Erro simulado no cadastro')
+      fakeUsers.value.push({
+        ...parsedData,
+        id: getRandomUUID(),
+        created_at: new Date().toISOString(),
+      })
       console.log(parsedData, 'enviado ao db', fakeUsers.value)
     } catch (err) {
       const e = err as ZodError
