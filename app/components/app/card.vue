15f25362551aa4df5483a6c7f55eb85f5b1e821a
betogroo <luizhumberto@gmail.com>
2024-09-02 16:02:03 -0300

    refactor: create app card component

---
+++ app/components/app/card.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+  interface Props {
+    title: string
+  }
+  defineProps<Props>()
+</script>
+
+<template>
+  <UCard class="my-2">
+    <template #header>
+      <h1 class="text-2xl font-semibold">{{ title }}</h1>
+    </template>
+    <div class="text-justify">
+      <slot />
+    </div>
+  </UCard>
+</template>
