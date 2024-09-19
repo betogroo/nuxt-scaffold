5ffe57be4397526d41d114bdf093edc260cf2e42
betogroo <luizhumberto@gmail.com>
2024-09-19 09:57:27 -0300

    refactor: change ui config of card

--- app/components/app/card.vue
+++ app/components/app/card.vue
@@ -7,7 +7,17 @@
 </script>
 
 <template>
-  <UCard class="my-2">
+  <UCard
+    class="my-2"
+    :ui="{
+      strategy: 'override',
+      divide: 'divide-none',
+      ring: 'ring-1 ring-green-500 dark:ring-green-800',
+      header: { padding: 'px-2 pt-4 pb-2' },
+      body: { padding: 'px-2' },
+      footer: { padding: 'px-2 pb-4' },
+    }"
+  >
     <template #header>
       <h1 class="text-2xl font-semibold">{{ title }}</h1>
       <h2
@@ -17,8 +27,10 @@
         {{ subtitle }}
       </h2>
     </template>
-    <div class="text-justify">
+
+    <div class="text-justify pa-0">
       <slot />
     </div>
+    <template #footer />
   </UCard>
 </template>
