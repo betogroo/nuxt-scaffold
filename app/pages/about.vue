4a72d6eec7103aefce9837838a6c3e4308330111
betogroo <luizhumberto@gmail.com>
2024-09-19 16:05:28 -0300

    add page transition

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -21,41 +21,43 @@
 </script>
 
 <template>
-  <section>
-    <AppCard title="About">
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta
-        recusandae atque ratione magnam, deleniti distinctio expedita, dolorum
-        ab explicabo eum ut reprehenderit quaerat consequuntur animi sunt, odio
-        laboriosam temporibus! Sint!
-      </p>
-    </AppCard>
-  </section>
-  <section>
-    <AppCard title="Pinia">
-      <p>
-        Caso possa ver o número
-        <span
-          v-if="store.displayNumber > 0"
-          class="text-2xl font-semibold"
-          >{{ store.displayNumber }}</span
-        >
-        significa que o Pinia está funcionando, já que manteve o valor mesmo
-        trocando de página. Retorne para o
-        <ULink
-          class="hover:underline font-semibold"
-          to="modules/counter"
-          >/contador</ULink
-        >
-        e confirme se éo mesmo número.
-      </p>
-    </AppCard>
-  </section>
-  <section>
-    <h1 class="page-title">Teste Modal</h1>
-    <UButton
-      label="Abrir Modal"
-      @click="openModal"
-    />
-  </section>
+  <div>
+    <section>
+      <AppCard title="About">
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta
+          recusandae atque ratione magnam, deleniti distinctio expedita, dolorum
+          ab explicabo eum ut reprehenderit quaerat consequuntur animi sunt,
+          odio laboriosam temporibus! Sint!
+        </p>
+      </AppCard>
+    </section>
+    <section>
+      <AppCard title="Pinia">
+        <p>
+          Caso possa ver o número
+          <span
+            v-if="store.displayNumber > 0"
+            class="text-2xl font-semibold"
+            >{{ store.displayNumber }}</span
+          >
+          significa que o Pinia está funcionando, já que manteve o valor mesmo
+          trocando de página. Retorne para o
+          <ULink
+            class="hover:underline font-semibold"
+            to="modules/counter"
+            >/contador</ULink
+          >
+          e confirme se éo mesmo número.
+        </p>
+      </AppCard>
+    </section>
+    <section>
+      <h1 class="page-title">Teste Modal</h1>
+      <UButton
+        label="Abrir Modal"
+        @click="openModal"
+      />
+    </section>
+  </div>
 </template>
