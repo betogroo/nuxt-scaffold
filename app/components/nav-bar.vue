a4824c9e98affed1b01f0ad08b26615d6cdacff0
betogroo <luizhumberto@gmail.com>
2024-10-04 08:48:16 -0300

    fix: add future link to profile

--- app/components/nav-bar.vue
+++ app/components/nav-bar.vue
@@ -11,8 +11,11 @@
     [
       {
         label: user.value?.email || '',
+        avatar: {
+          src: 'https://avatars.githubusercontent.com/u/739984?v=4',
+        },
         slot: 'account',
-        disabled: true,
+        click: () => console.log('Link to profile in the future'),
       },
     ],
     [
