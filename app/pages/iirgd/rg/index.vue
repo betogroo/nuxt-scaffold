aa2ea4de56db114773fceb36687162206fbc4714
betogroo <luizhumberto@gmail.com>
2024-10-18 14:04:55 -0300

    feat: add document demand form to index

--- app/pages/iirgd/rg/index.vue
+++ app/pages/iirgd/rg/index.vue
@@ -1,11 +1,35 @@
 <script setup lang="ts">
+  import type { DocumentDemandInsert } from '~/types'
+  const { handleError, showToast } = useHelpers()
+  const { demands, fetchDocumentDemands } = useDocumentDemand()
   const newRgModal = ref(false)
   const openModal = () => {
     newRgModal.value = true
   }
-  /* const closeModal = () => {
+  const closeModal = () => {
     newRgModal.value = false
-  } */
+  }
+
+  onMounted(async () => {
+    await fetchDocumentDemands()
+  })
+  const { addDocumentDemand, isPending } = useDocumentDemand()
+  const submitDocumentDemand = async (documentDemand: DocumentDemandInsert) => {
+    try {
+      const newDocumentDemand = await addDocumentDemand(documentDemand)
+      if (!newDocumentDemand) throw new Error('aqui deu erro')
+      showToast(
+        'success',
+        `Documento ${newDocumentDemand?.document_number} cadastrado com sucesso`,
+      )
+      closeModal()
+    } catch (err) {
+      const e = err as Error
+      const error = handleError(e)
+      showToast('error', error.message)
+      console.error(error)
+    }
+  }
 </script>
 
 <template>
@@ -22,10 +46,29 @@
         <AppCard title="Erro">5</AppCard>
       </div>
     </section>
-    <AppModal v-model="newRgModal" />
+    <AppModal
+      v-model="newRgModal"
+      title="Cadastrar Documento"
+    >
+      <FormDocumentDemand
+        :is-pending="isPending"
+        @on-submit="submitDocumentDemand"
+      />
+    </AppModal>
     <UButton
       icon="mdi-plus"
       @click="openModal"
     />
+
+    <div>
+      <ul>
+        <li
+          v-for="demand in demands"
+          :key="demand.id"
+        >
+          {{ demand.document_number }}
+        </li>
+      </ul>
+    </div>
   </UContainer>
 </template>
