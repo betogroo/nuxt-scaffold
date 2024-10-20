027d1928e63a73ab0d9bbc10056218834238f954
betogroo <luizhumberto@gmail.com>
2024-10-20 08:30:00 -0300

    feat: add name validation

--- app/schemas/documentDemandSchema.ts
+++ app/schemas/documentDemandSchema.ts
@@ -12,7 +12,7 @@ export const documentDemandRowSchema = z.object({
   created_at: z.string().nullable(),
   document_number: z.string(),
   id: z.number(),
-  name: z.string().min(1, 'Campo Obrigatório'),
+  name: z.string().min(1, 'Campo Obrigatório').trim(),
   status: demandStatusSchema,
   updated_at: z.string().nullable(),
   user_id: z.string(),
@@ -24,7 +24,20 @@ export const documentDemandInsertSchema = z.object({
     .string()
     .min(10, 'Digite o documento no padrão 00000000-0'),
   id: z.number().optional(),
-  name: z.string().min(1, 'Campo Obrigatório'),
+  name: z
+    .string()
+    .min(1, 'Campo Obrigatório')
+    .min(2, 'Parte do node deve conter no mínimo duas letras')
+    .trim()
+    .refine(
+      (data) => {
+        const words = data.split(/\s+/)
+        return words.length >= 2
+      },
+      {
+        message: 'Deve conter pelo menos o nome e sobrenome',
+      },
+    ),
   status: demandStatusSchema.optional(),
   updated_at: z.string().optional().nullable(),
   user_id: z.string().uuid(),
