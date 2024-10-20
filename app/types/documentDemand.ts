022306628d34158b6e4c50e648ab1a71a6e357cc
betogroo <luizhumberto@gmail.com>
2024-10-20 20:07:40 -0300

    refactor: schema import and export

--- app/types/documentDemand.ts
+++ app/types/documentDemand.ts
@@ -4,9 +4,11 @@ import type {
   documentDemandInsertSchema,
   documentDemandRowSchema,
   demandSiteSchema,
-} from './'
+  demandStatusSchema,
+} from '~/schemas'
 
 export type DemandSite = z.infer<typeof demandSiteSchema>
+export type DemandStatus = z.infer<typeof demandStatusSchema>
 
 export type DocumentDemandInsert = z.infer<typeof documentDemandInsertSchema>
 export type DocumentDemandRow = z.infer<typeof documentDemandRowSchema>
