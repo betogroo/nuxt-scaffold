8ec865e9f2b183f63dc3632d6bf23e0d525b7bf1
betogroo <luizhumberto@gmail.com>
2024-12-02 14:14:45 -0300

    refactor: rename insert

--- app/composables/useDocumentDemand.ts
+++ app/composables/useDocumentDemand.ts
@@ -111,13 +111,11 @@ const useDocumentDemand = () => {
     }, 'addDocumentDemand')
   } */
 
-  const {
-    insertPending: documentDemandPending,
-    insertData: insertDocumentDemand,
-  } = useGenericInsert<DocumentDemandInsert, DocumentDemandRow>(
-    'document_demand',
-    documentDemandInsertSchema,
-  )
+  const { insertPending: documentDemandPending, insert: insertDocumentDemand } =
+    useGenericInsert<DocumentDemandInsert, DocumentDemandRow>(
+      'document_demand',
+      documentDemandInsertSchema,
+    )
 
   return {
     insertDocumentDemand,
