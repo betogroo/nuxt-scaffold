623a252dc8e64a9bf34e168f6df17f85456c3e99
betogroo <luizhumberto@gmail.com>
2024-12-04 09:14:13 -0300

    refactor: handle error

--- app/types/helpers.ts
+++ app/types/helpers.ts
@@ -1,6 +1,8 @@
 import type { z } from 'zod'
 import type { pendingStateSchema, pendingOptionsSchema } from '~/schemas'
+
 import type { Database } from '~/types/supabase'
+export type { PostgrestError } from '@supabase/supabase-js'
 
 export type PendingState = z.infer<typeof pendingStateSchema>
 export type PendingOptions = z.infer<typeof pendingOptionsSchema>
