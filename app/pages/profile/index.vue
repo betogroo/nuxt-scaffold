34ba0a71c2f09d0c28794a9df88bea04248a6aa8
betogroo <luizhumberto@gmail.com>
2024-12-04 10:31:04 -0300

    feat: profile screen

--- app/pages/profile/index.vue
+++ app/pages/profile/index.vue
@@ -3,6 +3,7 @@
   const { handleError } = useHelpers()
   const { profile, getProfile } = useProfile()
   const error = ref('')
+  const editMode = ref(false)
 
   try {
     await getProfile()
@@ -16,9 +17,37 @@
   <div v-if="!profile || !user">
     <ErrorDataInvalid :message="error" />
   </div>
-  <div v-else>
-    <div>Profile</div>
-    <div v-if="user">{{ user.email }}</div>
-    <div>{{ profile }}</div>
+  <div
+    v-else
+    class="container mx-auto p-10"
+  >
+    <div v-if="!editMode">
+      <div class="text-center">
+        <UAvatar
+          alt="Avatar"
+          size="3xl"
+          src="https://avatars.githubusercontent.com/u/739984?v=4"
+          :ui="{
+            size: {
+              '3xl': 'h-44 w-44 text-3xl',
+            },
+          }"
+        />
+      </div>
+      <div class="text-center text-2xl">
+        {{ profile.name || 'Inserir Nome' }}
+      </div>
+      <div class="text-center font-light">{{ profile.email }}</div>
+    </div>
+    <div
+      v-else
+      class="text-center"
+    >
+      Fomrmulario
+    </div>
+
+    <div class="text-center">
+      <UButton @click="editMode = !editMode">Editar Perfil</UButton>
+    </div>
   </div>
 </template>
